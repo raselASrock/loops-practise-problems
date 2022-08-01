bb05cba68eae522b3c7d12ac960c47dfc41b6fad
raselASrock <mdraselm413@gmail.com>
2022-08-02 01:37:15 +0600

    solution

---
+++ problem-solution.js
@@ -0,0 +1,21 @@
+/********Practise Problem Solution-01 *******/
+
+var moneyGiven = 1000;
+var orangeAppleCost = 700;
+var totalCost = orangeAppleCost ;
+var shopkeeperReturn = moneyGiven - totalCost ;
+console.log (shopkeeperReturn);
+
+/********Practise Problem Solution-02 *******/
+
+var mathmatics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totalMark = mathmatics + biology + chemistry + physics + bangla ;
+var totalSubject = 5; 
+var fiveAvg = totalMark / totalSubject;
+console.log(fiveAvg.toFixed(2));
+
+/********Practise Problem Solution-03 *******/
--- reverse.js
+++ reverse.js
@@ -1,5 +1,15 @@
-var num = 0;
-while( num < 10){
+// var num = 0;
+// while( num < 10){
+//     console.log(num)
+//     num++
+// }
+
+var num = 10;
+while( num >= 1){
+    console.log(num);
+    num--
+}
+
+for( var num = 10; num >=1; num--){
     console.log(num)
-    num++
 }
